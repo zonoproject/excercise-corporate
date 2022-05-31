6df664bfcb4a894b9737ef7e8945053809bf21e3
zonoproject <zonoproject@gmail.com>
2022-05-31 19:45:48 +0900

    fix-js-third

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
   // インクルード
-fetch("header.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("header").innerHTML = data;
-    // ドロワーメニュー
-    const target = document.getElementById("menu");
-    target.addEventListener('click', () => {
-      target.classList.toggle('open');
-      const nav = document.getElementById("nav");
-      nav.classList.toggle('in');
+  fetch("header.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("header").innerHTML = data;
+      // ドロワーメニュー
+      const target = document.getElementById("menu");
+      target.addEventListener('click', () => {
+        target.classList.toggle('open');
+        const nav = document.getElementById("nav");
+        nav.classList.toggle('in');
+      });
     });
-  });
 
   fetch("footer.html")
     .then(response => {
@@ -33,5 +33,5 @@ fetch("header.html")
       document.querySelector(".page-contact__wrap").innerHTML = data;
     })
     .catch(error => {
-      // ignore
+      return null;
     });
