3b9419112e9d777afe34667c33af1a6834fe5bda
zonoproject <zonoproject@gmail.com>
2022-05-31 19:38:53 +0900

    fix-js-second

--- js/main.js
+++ js/main.js
@@ -1,40 +1,37 @@
-// インクルード
+  // インクルード
 fetch("header.html")
   .then(response => {
     return response.text()
   })
   .then(data => {
     document.querySelector("header").innerHTML = data;
+    // ドロワーメニュー
+    const target = document.getElementById("menu");
+    target.addEventListener('click', () => {
+      target.classList.toggle('open');
+      const nav = document.getElementById("nav");
+      nav.classList.toggle('in');
+    });
   });
 
-fetch("footer.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("footer").innerHTML = data;
-  });
+  fetch("footer.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("footer").innerHTML = data;
+    });
 
-fetch("page_contact.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    if (!data == null) {
+  fetch("page_contact.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      if (!data == null) {
+        // ignore
+      }
+      document.querySelector(".page-contact__wrap").innerHTML = data;
+    })
+    .catch(error => {
       // ignore
-    }
-    document.querySelector(".page-contact__wrap").innerHTML = data;
-  })
-  .catch(error => {
-    // ignore
-  });
-
-// ドロワーメニュー
-window.addEventListener('load', function () {
-  const target = document.getElementById("menu");
-  target.addEventListener('click', () => {
-    target.classList.toggle('open');
-    const nav = document.getElementById("nav");
-    nav.classList.toggle('in');
-  });
-});
+    });
